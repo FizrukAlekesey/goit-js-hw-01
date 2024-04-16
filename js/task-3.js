959f7cc178a6090414145b2a9e4ebbe1a3f36309
Fizruk <fizrukprogram@gmail.com>
2024-04-16 21:26:49 +0200

    2

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function getElementWidth (content, padding, border) {
      
-    return  content + padding * padding  + border * border ;
+    return  Number.parseFloat(content) + Number.parseFloat(padding) * 2  + Number.parseFloat( border) * 2 ;
     console.log()
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
